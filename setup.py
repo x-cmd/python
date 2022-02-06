90632411e0d6bb37d57599f8b4f468c019e6a8cc
edwinjhlee <edwin.jh.lee@gmail.com>
2022-02-06 13:44:23 +0800

    ...

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="x-cmd",
+    version="0.0.1",
+    author="L",
+    author_email="l@x-cmd.com",
+    description="X Cmd Wrapper",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/x-cmd/python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
