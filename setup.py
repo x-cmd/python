596d7884b19c23436f18c84ff647156d39d1c6ba
edwinjhlee <edwin.jh.lee@gmail.com>
2022-02-06 14:08:55 +0800

    ...

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="x-cmd",
+    name="x",
     version="0.0.1",
     author="L",
     author_email="l@x-cmd.com",
